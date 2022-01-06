3bc2da8601bc342deefb4e0f419f513ecb822b2c
otwoma <otwomadennis365@gmail.com>
2022-01-06 18:11:15 +0300

    started building the video cards

--- src/components/Home/Previews/PreviewVideos/AllPreviewVideos.jsx
+++ src/components/Home/Previews/PreviewVideos/AllPreviewVideos.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import PreviewVideo from "./PreviewVideo";
+import tourismZone from '../../../../assets/images/zone-5.jpg';
+import travelifyLogo from '../../../../assets/images/zone-3.jpg';
 
 const AllPreviewVideos = () => {
     return(
         <section className="all-preview-videos">
-            <PreviewVideo />
+            <PreviewVideo
+                thumbnailPhotoPath={tourismZone}
+                playTime="25:30"
+                videoTitle="top ten vaccation sites of all time for you and your family in this festive season"
+                channelName="travelify"
+                views="2M"
+                releaseDate="2 months ago"
+                channelAvartarPath={travelifyLogo}
+             />
         </section>
     )
 }
--- src/components/Home/Previews/PreviewVideos/PreviewVideo.jsx
+++ src/components/Home/Previews/PreviewVideos/PreviewVideo.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
-const PreviewVideo = () => {
+const PreviewVideo = ({videoPath, thumbnailPhotoPath, videoTitle, channelName, views, releaseDate, playTime, channelAvartarPath}) => {
     return(
-        <a href="yes" className="preview-video">this is a video link</a>
+        <a href="yes" className="preview-video">
+            <div className="preview-video__thumbnail-container">
+                <img src={thumbnailPhotoPath} alt={`${videoTitle} thumbnail representation`} className="preview-video__thumbnail-container--img" />
+                <span className="preview-video__play-time">{playTime}</span>
+            </div>
+            <div className="preview-video__body">
+                <div className="preview-video__avartar">
+                    <img src={channelAvartarPath} alt={`${channelName} avartar`} className="preview-video__avartar--img" />
+                </div>
+                <div className="preview-video__details">
+                    <h5 className="preview-video__details--title">{videoTitle}</h5>
+                    <p className="preview-video__details--channel">{channelName}</p>
+                    <p className="preview-video__misc">
+                        <span className="preview-video__misc--views">{views} views</span>
+                        <span>.</span>
+                        <span className="preview-video__misc--release">{releaseDate}</span>
+                    </p>
+                </div>
+            </div>
+        </a>
     )
 }
 export default PreviewVideo;
